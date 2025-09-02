c229d2355628f06586dcaffd421ac60d66940a6c
ajay <ajaypathak2527@gmail.com>
2025-09-02 22:48:56 +0530

    update placeholder image

--- client/src/lib/constant.ts
+++ client/src/lib/constant.ts
@@ -1,1 +1,1 @@
-export const FAlLLBACK_IMAGE = "https://media.istockphoto.com/id/1409329028/vector/no-picture-available-placeholder-thumbnail-icon-illustration-design.jpg?s=612x612&w=0&k=20&c=_zOuJu755g2eEUioiOUdz_mHKJQJn-tDgIAhQzyeKUQ="
+export const FAlLLBACK_IMAGE = "https://placehold.co/600x400.png"
